838ef258a99a30ff7499f572b4bc127298b0cf29
DUR-cloud <elektro.ademaj@gmail.com>
2025-09-08 11:23:32 +0200

    feat: Notiz & Krankmeldung je Mitarbeiter

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b150g0op7no"
+    "revision": "0.9g4scl1qguo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
