89c6928a1fd423441eed5741e6f17f51d3718878
DUR-cloud <elektro.ademaj@gmail.com>
2025-09-12 15:16:39 +0200

    purge pwa imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,12 @@
+// src/main.jsx
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// PWA: Service Worker Registrierung (autoUpdate) – NACH dem render()!
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", async () => {
-    try {
-      const { registerSW } = await import("virtual:pwa-register");
-      registerSW({ immediate: true });
-    } catch {
-      // ignore
-    }
-  });
-}
